4a93280a843e0255d7690e03c9fed4501312b5b0
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2022-03-10 19:23:00 -0500

    clearing

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 //bring apollo
 const { ApolloServer } = require('apollo-server-express');
-//const { typeDefs, resolvers } = require('./schemas');
-//const {authMiddleware} = require('../utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+const {authMiddleware} = require('../utils/auth');
 
 const db = require('./config/connection');
 //const routes = require('./routes');
